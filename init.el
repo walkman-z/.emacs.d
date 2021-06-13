1fa5ee254ab4bd4cabe109ba3fbc60a834d56a1b
walkman-win <walkman-z@foxmail.com>
2021-06-13 14:44:33 +0800

    win test

--- init.el
+++ init.el
@@ -16,13 +16,49 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 
+;;设置软件源				;
+;; (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+;; 			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+;; 			 ("org" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
 
-(require 'init-elpa)			;软件源及包管理器初始化
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ;; ("melpa-stable" . "https://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ("org" . "https://mirrors.ustc.edu.cn/elpa/org/")))
 
-(require 'init-const)
-(require 'init-kbd)
-(require 'init-unicode)
-(require 'init-startup)					;
+
+;;不检查校验签名
+;; (setq package-check-signature nil)
+
+
+;;初始化包管理器
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
+;;刷新软件源索引				;
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;usepackage
+
+(setq use-package-always-ensure t
+       use-package-always-defer t
+       use-package-always-demand nil
+       use-package-expand-minimally t
+       use-package-verbose t)
+
+(require 'use-package)
+
+
+;; (require 'init-elpa)			;软件源及包管理器初始化
+
+;; (require 'init-const)
+;; (require 'init-kbd)
+;; (require 'init-unicode)
+;; (require 'init-startup)					;
 
 
 
